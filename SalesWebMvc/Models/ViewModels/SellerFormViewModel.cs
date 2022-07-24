356e70cef26324dce4a1a104d41f5aaf8247357e
endrewmota <endrew.desousa@gmail.com>
2022-07-24 01:23:18 -0400

    Update seller and custom service exception

--- SalesWebMvc/Models/ViewModels/SellerFormViewModel.cs
+++ SalesWebMvc/Models/ViewModels/SellerFormViewModel.cs
@@ -2,7 +2,7 @@
 
 namespace SalesWebMvc.Models.ViewModels
 {
-    public class SallerFormViewModel
+    public class SellerFormViewModel
     {
         public Seller Seller { get; set; }
         public ICollection<Department> Departments { get; set; }
